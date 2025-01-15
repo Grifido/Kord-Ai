4ae0ce6f47f98fb3bab2b5b3aa70f5552fd9418a
Vadict_nova <vadictvadictnova@gmail.com>
2025-01-15 10:35:20 +0300

    Update Config.js Vadict_nova
    Vadict_nova benah

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
 // hey there, kindly read this file till the end for better configuration
 
 global.settings = {
-  OWNER_NUMBERS: "2349067339193", // your number with country code
-  OWNER_NAME: 'Miracle', // your owner name 
-  BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: 'your_sessionid', // replace your_sessionid with yours and make sure its inside rhe quotes
+  OWNER_NUMBERS: "25426934375", // your number with country code
+  OWNER_NAME: 'Vadict_nova, // your owner name 
+  BOT_NAME: "benah", // your preferred bot name
+  SESSION_ID: 'kord_ai-WnGFayh50o4I', // replace your_sessionid with yours and make sure its inside rhe quotes
   INVINCIBLE_MODE: false, // toogle invincible mode 
   WORK_MODE: "Public",
   TIME_ZONE: "Africa/Lagos",
